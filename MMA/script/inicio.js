b5f27ee61e2513847f35ff0d8216c0d7a180aeda
DaavidBaal <david.bartolome.alonso@tajamar365.com>
2017-06-06 19:50:17 +0200

    pruebas diseño

--- MMA/script/inicio.js
+++ MMA/script/inicio.js
@@ -4,6 +4,9 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+/************************
+ * Funcion noticias
+******************** */
 function noticias() {
     document.getElementById("noticias").classList.toggle("mostrar");
 }
@@ -16,6 +19,17 @@ window.onclick = function(e) {
     var myDropdown = document.getElementById("myDropdown");
       if (myDropdown.classList.contains('show')) {
         myDropdown.classList.remove('show');
+      }
+  }
+}
+/*********************************************
+ * Cierra el menu si clicas fuera de noticias
+ ********************************************/
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var noticias = document.getElementById("noticias");
+      if (noticias.classList.contains('show')) {
+        noticias.classList.remove('show');
       }
   }
 }
