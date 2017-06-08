8691befb0ae0a0b6e6328310f865e034ebf53c0f
DaavidBaal <david.bartolome.alonso@tajamar365.com>
2017-06-08 17:13:42 +0200

    progress

--- 07-Design/Examen 3/script/menu.js
+++ 07-Design/Examen 3/script/menu.js
@@ -16,20 +16,3 @@ window.onclick = function(e) {
 }
 
 
-/***************************
- * Funcion para Progresss
- *******************************/
-function move() {
-  var elem = document.getElementById("barra");   
-  var width = 10;
-  var id = setInterval(frame, 15);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width++; 
-      elem.style.width = width + '%'; 
-      elem.innerHTML = width * 1  + '%';
-    }
-  }
-}
